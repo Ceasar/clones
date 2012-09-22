cd986a6fc59f3e6a10cfd7172b61f0fe85622c50
Ceasar Bautista <cbautista2010@gmail.com>
2012-09-22 11:36:26 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,19 @@
+Goals
+=====
+Floats, Display
+
+Stacking Context
+
+Positioning (and challenges)
+
+Repositing items on scroll (focus mode header)
+
+JS console bug
+
+Using SASS mixins properly
+
+Semantic varibales
+
+Times when you need to use an extra div
+
+Modals?
---
+++ pinterest/app.py
@@ -0,0 +1,25 @@
+import csv
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+def parse_csv(filename):
+    """Read data from a CSV.
+    This will return a list of dictionaries, with key-value pairs corresponding
+    to each column in the parsed csv.
+    """
+    with open(filename, 'rb') as f:
+        return list(csv.DictReader(f))
+
+
+@app.route('/')
+def hello_world():
+    pins = list(parse_csv("data/pins.csv"))
+    print pins
+    return render_template('index.html', pins=pins)
+
+if __name__ == '__main__':
+    app.run(debug=True)
